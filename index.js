45d258477ccd0b9cbe666c0d34d1de3697694716
Nitin Patil <77918399+Nitin-patil2209@users.noreply.github.com>
2021-03-23 15:17:47 +0530

    Drum game
    this is gaming wwebsite.

---
+++ index.js
@@ -0,0 +1,92 @@
+
+var numberof=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberof;i++){
+  document.querySelectorAll("button")[i].addEventListener("click" ,firstf);
+function firstf()
+{
+  var inner = this.innerHTML;
+  MakeSound(inner);
+  anime(inner);
+}
+
+}
+
+document.addEventListener("keypress", function(event){
+     MakeSound(event.key);
+     anime(event.key);
+});
+function MakeSound(key){
+
+     switch(key)
+  {
+     case "w": var sound= new Audio("sounds/tom-1.mp3");
+                 sound.play();
+     break;
+     case "a": var sound= new Audio("sounds/tom-2.mp3");
+                 sound.play();
+     break;
+     case "s": var sound= new Audio("sounds/tom-3.mp3");
+                 sound.play();
+     break;
+     case "d": var sound= new Audio("sounds/tom-4.mp3");
+                 sound.play();
+     break;
+     case "j": var sound= new Audio("sounds/crash.mp3");
+                 sound.play();
+     break;
+     case "k": var sound= new Audio("sounds/kick-bass.mp3");
+                 sound.play();
+     break;
+     case "l": var sound= new Audio("sounds/snare.mp3");
+                 sound.play();
+     break;
+     default: console.log("not found");
+
+  }
+}
+// for(var i=0;i<numberof;i++){
+// 	document.querySelectorAll("button")[i].addEventListener("click" ,firstf);
+// function firstf()
+// {
+// 	var inner = this.innerHTML;
+// 	switch(inner)
+// 	{
+//      case "w": var sound= new Audio("sounds/tom-1.mp3");
+//                	sound.play();
+//      break;
+//      case "a": var sound= new Audio("sounds/tom-2.mp3");
+//                	sound.play();
+//      break;
+//      case "s": var sound= new Audio("sounds/tom-3.mp3");
+//                	sound.play();
+//      break;
+//      case "d": var sound= new Audio("sounds/tom-4.mp3");
+//                	sound.play();
+//      break;
+//      case "j": var sound= new Audio("sounds/crash.mp3");
+//                	sound.play();
+//      break;
+//      case "k": var sound= new Audio("sounds/kick-bass.mp3");
+//                	sound.play();
+//      break;
+//      case "l": var sound= new Audio("sounds/snare.mp3");
+//                	sound.play();
+//      break;
+//      default: console.log("not found");
+
+// 	}
+// }
+
+// }
+// document.addEventListener("keypress", function(event){
+//      console.log(event.key);
+// })
+
+function anime(keys){
+ var  activebutton=document.querySelector("." + keys);
+ activebutton.classList.add("pressed");
+setTimeout(function(){
+activebutton.classList.remove("pressed");
+
+},100);
+}
